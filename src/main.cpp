73cc52bdd73f6f939fbd5aeb35ca5ed1817a81bc
vez767 <wa3960f@greenwich.ac.uk>
2026-03-05 13:59:28 +0000

    feat: implement HC-SR04 ultrasonic sensor logic

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance; 
+
+void setup() {
+Serial.begin(9600);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);   
+}
+
+void loop() {
+
+digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  delay(500);
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
+}
