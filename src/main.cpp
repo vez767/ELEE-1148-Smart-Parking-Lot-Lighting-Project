9e2f6fd139f5a105a1785544e8d6869d3af7901f
vez767 <wa3960f@greenwich.ac.uk>
2026-03-06 15:36:22 +0000

    refactor:initialize digital filter for LDR logic

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,18 @@ const int LDR_PIN = A0;
 
 float duration, distance; 
 
+int digital_filter(){
+  long total_ldrValue = 0;
+
+  for(int i = 0; i < 15; i++){
+    total_ldrValue += analogRead(LDR_PIN);
+    delay(10);
+  }
+  int avg = total_ldrValue/15;
+  return avg;
+}
+
+
 void setup() {
 Serial.begin(9600);
 pinMode(trigPin, OUTPUT);
@@ -26,11 +38,10 @@ digitalWrite(trigPin, LOW);
   delay(500);
   Serial.print("Distance: ");
   Serial.println(distance);
-  delay(100);
-
-  int ldrValue = analogRead(LDR_PIN); 
+  delay(100); 
+  int avg_ldrValue = digital_filter();
   Serial.print("LDR Value: ");
-  Serial.println(ldrValue);
+  Serial.println(avg_ldrValue);
   delay(1000);
   analogWrite(LEDPIN, 255);   
  
